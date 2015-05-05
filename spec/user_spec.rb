623b294752c44d57c6dda1b83ccab0ede43dc312
Rizwan Ali <rizcube@gmail.com>
2015-05-05 17:18:18 +0100

    added secrets.yml all the test are now passing

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-describe User, :type => model do
-  it { is_expected.to have_many :reviewed restaurants, through: :reviews  }
+describe User do
+  it { is_expected.to have_many :reviewed_restaurants}
 end
