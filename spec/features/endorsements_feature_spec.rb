26b274b7b6686e661cf7ac8324e656d7097b0531
Rizwan Ali <rizcube@gmail.com>
2015-05-11 16:45:20 +0100

    added partial for the form

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 feature 'endorsing reviews' do
+   before do
+
+  end
+
   before do
     kfc = Restaurant.create(name: 'KFC')
     kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
@@ -12,4 +16,23 @@ feature 'endorsing reviews' do
     expect(page).to have_content('1 endorsement')
   end
 
+  xit 'a user can endorse a review, which increments the endorsement count', js: true do
+    visit '/restaurants'
+    click_link 'Endorse'
+    expect(page).to have_content("1 endorsement")
+  end
 end
+
+def sign_up
+  visit('/')
+  click_link('Sign up')
+  fill_in('Email', with: 'test@example.com')
+  fill_in('Password', with: 'testtest')
+  fill_in('Password confirmation', with: 'testtest')
+  click_button('Sign up')
+end
+
+
+
+
+
